fae477c425f0af91f2c6b111ba4a5cacd742854d
lovezsh <shuxhan@163.com>
2023-04-06 20:35:51 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+## bilibilig-uploads-img
+
+基于 https://github.com/xlzy520/bili-short-url 二次开发。
+
+使用 vue + ejs 框架开发，使用 gulp 进行构建。
+
+上传图片，查看已上传历史列表。
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const { series, parallel, watch, dest, src } = require('gulp');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+const { rollup } = require('rollup');
+const { terser } = require('rollup-plugin-terser');
+const commonjs = require('@rollup/plugin-commonjs');
+const resolve = require('@rollup/plugin-node-resolve').default;
+const babel = require('@rollup/plugin-babel').default;
+const connect = require('gulp-connect');
+
+// babel配置
+const babelConfig = {
+  babelHelpers: 'bundled',
+  ignore: ['node_modules'],
+};
+
+// 清除dist目录
+function distClean() {
+  return src('dist', { allowEmpty: true }).pipe(clean());
+}
+
+// 移动html模板
+function html() {
+  return src('src/*.html').pipe(dest('dist'));
+}
+
+function compileEjs() {
+  return src('src/pages/*.ejs')
+    .pipe(ejs())
+    .pipe(rename({ extname: '.html' }))
+    .pipe(dest('dist'));
+}
+
+function publicFile() {
+  return src('public/**').pipe(dest('dist'));
+}
+
+// 编译scss文件
+// function compileSass() {
+//   return src('src/style/style.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(autoprefixer())
+//     .pipe(minify({ compatibility: 'ie11' }))
+//     .pipe(dest('dist/css'));
+// }
+
+function server(cb) {
+  connect.server({
+    root: 'dist',
+    port: '1998',
+    livereload: true,
+  });
+  cb();
+}
+
+function srcWatch() {
+  // watch(['src/*.html'], series(html, reload));
+  watch(['src/pages/**/*.ejs'], series(compileEjs, reload));
+  // watch(['src/adminPages/**/*.ejs'], series(compileEjs2, reload));
+  // watch(['src/servePages/**/*.ejs'], series(compileEjs3, reload));
+  watch(['public/**'], series(publicFile, reload));
+  // watch(['src/index.js'], series(compileJs, reload));
+  // watch(['src/utils/*.js'], series(compileJs, reload));
+  // watch(['src/style/**/*.scss'], series(compileSass, reload));
+}
+
+function reload() {
+  return src('dist/*.html').pipe(connect.reload());
+}
+
+exports.build = series(distClean, parallel( compileEjs, publicFile));
+
+exports.dev = series(distClean, parallel( compileEjs, publicFile), server, srcWatch);
